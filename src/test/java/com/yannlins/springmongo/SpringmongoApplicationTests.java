f1092b988d1559aae79dc2d24c6b4635367edd7e
Yann Lins <ymclins@gmail.com>
2020-05-20 20:09:32 -0300

    Projeto criado

---
+++ src/test/java/com/yannlins/springmongo/SpringmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yannlins.springmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
